8b673e8d2df65627068a1f5bcee99e5792aba2ce
sidajk00 <sidajk00@gmail.com>
2024-08-16 18:17:41 +0900

    update

--- UISample/UI/Main/Practice/ToastModifier.swift
+++ UISample/UI/Main/Practice/ToastModifier.swift
@@ -10,19 +10,31 @@ import SwiftUI
 struct ToastModifier: ViewModifier {
 
     @Binding var isShow: Bool
+    @State var opacity: CGFloat = 0
+    @State var animated: Bool = false
+    
     func body(content: Content) -> some View {
         content
             .overlay {
                 ZStack {
                     mainView()
+                        .fadeTransition(isVisible: isShow, duration: 0.25)
                 }
             }
+            .onChange(of: isShow) { value in
+                if isShow {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        isShow = false
+                    }
+                }
+            }
+            .animation(.default, value: 1)
     }
     
     @ViewBuilder func mainView() -> some View {
-        if isShow {
+        //if isShow {
             ToastView()
-        }
+        //}
     }
 }
 
