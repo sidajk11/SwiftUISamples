dc89fb1de0b89d09ec26261687710405e530c80d
sidajk00 <sidajk00@gmail.com>
2024-08-20 11:58:26 +0900

    update

--- UISample/UI/Main/LayoutTestView.swift
+++ UISample/UI/Main/LayoutTestView.swift
@@ -9,23 +9,35 @@ import SwiftUI
 
 struct LayoutTestView: View {
     var body: some View {
-        ZStack(alignment: .topLeading) {
+        Rectangle()
+            .foregroundStyle(.appGray300)
+            .overlay(content)
+        //content
+    }
+    
+    var content: some View {
+        ZStack(alignment: .center) {
             Rectangle()
+                .fill(.red)
                 .foregroundStyle(.red)
             //.position(popoverFrame.origin)
             //.frame(width: popoverFrame.width, height: popoverFrame.height)
-                .offset(CGSize(width: 100, height: 100))
-                .background(.blue)
-            //.position(CGPoint(x: 150 + 50, y: 150 + 50))
-                .offset(CGSize(width: 100, height: 100))
-                .background(.green)
-                .frame(width: 100, height: 100)
-                .offset(CGSize(width: 100, height: 100))
-            //.position(CGPoint(x: 150 + 50, y: 150 + 50))
+//                .offset(CGSize(width: 10, height: 0))
+//                .background(.blue)
+//                .position(CGPoint(x: 150 + 50, y: 150 + 50))
+//                .offset(CGSize(width: 10, height: 0))
+//                .background(.green)
+                .frame(width: 300, height: 300)
+                .offset(CGSize(width: 0, height: 0))
+                //.position(CGPoint(x: 50, y: 50))
+            
+            Rectangle()
+                .foregroundStyle(.yellow)
+                .frame(width: 140, height: 140)
         }
     }
 }
 
 #Preview {
-    LayoutTestView(viewModel: .preview)
+    LayoutTestView()
 }
